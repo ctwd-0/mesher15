9d8ec9788ef30e0226a4efcdbd5f83006eb23af9
ctwd-0 <suncong10@gmail.com>
2017-10-16 04:45:40 +0800

    马勒戈壁的。差飞了。究竟错在哪？

--- mesher/opencascade_simplifier.cpp
+++ mesher/opencascade_simplifier.cpp
@@ -13,10 +13,12 @@
 #include <opennurbs.h>
 
 #include <TColgp_Array2OfPnt.hxx>
+#include <TColgp_Array2OfPnt2d.hxx>
 #include <TColStd_Array2OfReal.hxx>
 #include <TColStd_Array1OfReal.hxx>
 #include <TColStd_Array1OfInteger.hxx>
 #include <BSplCLib.hxx>
+#include <Geom2d_BSplineCurve.hxx>
 #include <Geom_BSplineSurface.hxx>
 #include <GeomTools_SurfaceSet.hxx>
 #include <Precision.hxx>
@@ -56,6 +58,8 @@
 #include <ProjLib.hxx>
 
 #include <ProjLib_CompProjectedCurve.hxx>
+
+#include <Geom2dAPI_ProjectPointOnCurve.hxx>
 
 
 ////#pragma comment(lib, "TKBin.lib")
@@ -121,6 +125,7 @@
 #pragma comment(lib, "TKBRep.lib")
 #pragma comment(lib, "TKernel.lib")
 #pragma comment(lib, "TKG3d.lib")
+#pragma comment(lib, "TKG2d.lib")
 #pragma comment(lib, "TKGeomBase.lib")
 #pragma comment(lib, "TKMath.lib")
 #pragma comment(lib, "TKTopAlgo.lib")
@@ -277,6 +282,48 @@ using namespace std;
 //
 
 
+Handle_Geom2d_BSplineCurve convert_opennurbs_2d_curve_to_occt_2d_curve(const ON_NurbsCurve* curve) {
+	TColgp_Array1OfPnt2d poles(1, curve->CVCount());
+	TColStd_Array1OfReal weights(1, curve->CVCount());
+
+	TColStd_Array1OfReal knot_sequence(1, curve->KnotCount() + 2);
+
+	// Control point and its weight.
+	for (int i = 0; i < curve->CVCount(); ++i) {
+		if (curve->IsRational()) {
+			ON_4dPoint aPole;
+			curve->GetCV(i, aPole);
+			poles.SetValue(i + 1, gp_Pnt2d(aPole.x / aPole.w, aPole.y / aPole.w));
+			weights.SetValue(i + 1, aPole.w);
+		}
+		else {
+			ON_3dPoint aPole;
+			curve->GetCV(i, aPole);
+			poles.SetValue(i + 1, gp_Pnt2d(aPole.x, aPole.y));
+			weights.SetValue(i + 1, 1.);
+		}
+	}
+
+	// Knot vector and its multiplicity.
+	for (int i = 0; i < curve->KnotCount(); ++i) {
+		knot_sequence.SetValue(i + 2, curve->Knot(i));
+	}
+
+	knot_sequence.SetValue(knot_sequence.Lower(), curve->Knot(0));
+	knot_sequence.SetValue(knot_sequence.Upper(), curve->Knot(curve->KnotCount() - 1));
+
+	TColStd_Array1OfReal knots(1, BSplCLib::KnotsLength(knot_sequence, curve->IsPeriodic()));
+	TColStd_Array1OfInteger muls(1, knots.Upper());
+
+	BSplCLib::Knots(knot_sequence, knots, muls);
+
+	Handle_Geom2d_BSplineCurve occt_curve = new Geom2d_BSplineCurve(
+		poles, weights, knots, muls,
+		curve->Degree(), curve->IsPeriodic());
+
+	return occt_curve;
+}
+
 Handle_Geom_BSplineCurve convert_opennurbs_curve_to_occt_curve(const ON_NurbsCurve* curve) {
 	TColgp_Array1OfPnt poles(1, curve->CVCount());
 	TColStd_Array1OfReal weights(1, curve->CVCount());
@@ -374,111 +421,150 @@ Handle_Geom_BSplineSurface convert_opennurbs_surface_to_occt_surface(const ON_Nu
 	return occt_surface;
 }
 
-TopoDS_Edge convert_opennurbs_edge_to_occt_edge(ON_BrepEdge* edge, bool reverse) {
+//TopoDS_Edge convert_opennurbs_edge_to_occt_edge(ON_BrepEdge* edge, bool reverse) {
+//	BRep_Builder brep_builder;
+//	
+//	auto occt_curve = convert_opennurbs_curve_to_occt_curve(edge->NurbsCurve());
+//
+//	TopoDS_Edge occt_edge = BRepBuilderAPI_MakeEdge(occt_curve);
+//
+//	auto p0 = edge->Vertex(0)->Point();
+//	auto p1 = edge->Vertex(1)->Point();
+//
+//
+//
+//	//cout << edge->ProxyCurveIsReversed() << endl;
+//
+//	//马勒戈壁的。
+//	//loop中边的顺序可能会有反的。
+//	//而且看起来一定会有。
+//	//原因是在trim层，可能把3d curve反转。
+//	//问题仍然没有解决。
+//
+//	TopoDS_Vertex occt_v0 = BRepBuilderAPI_MakeVertex(gp_Pnt(p0.x, p0.y, p0.z));
+//	TopoDS_Vertex occt_v1 = BRepBuilderAPI_MakeVertex(gp_Pnt(p1.x, p1.y, p1.z));
+//	brep_builder.Add(occt_edge, occt_v0);
+//	brep_builder.Add(occt_edge, occt_v1);
+//
+//	//cout << p0.x+300 << " " << p0.y+2200 << " " << 0 << endl;
+//	//cout << p1.x+300 << " " << p1.y+2200 << " " << 0 << endl;
+//	//cout<<endl;
+//
+//
+//	if (reverse) {
+//		occt_edge.Reverse();
+//	}
+//
+//	
+//	return occt_edge;
+//}
+
+//static int curve_proxy_c = 0;
+//static int on_surface_c = 0;
+//static int line_c = 0;
+//static int arc_c = 0;
+//static int polyline_c = 0;
+//static int polycurve_c = 0;
+//static int nurbs_c = 0;
+
+//int opennurbs_curve2d_tester(ON_Curve* curve_2d) {
+//
+//	//auto curve_proxy = dynamic_cast<ON_CurveProxy*>(curve_2d); //checked
+//	//auto curve_on_surface = dynamic_cast<ON_CurveOnSurface*>(curve_2d); //checked
+//	//auto line = dynamic_cast<ON_LineCurve*>(curve_2d); //checked
+//	//auto arc = dynamic_cast<ON_ArcCurve*>(curve_2d); //checked
+//	//auto polyline = dynamic_cast<ON_PolylineCurve*>(curve_2d); //checked
+//	//auto polycurve = dynamic_cast<ON_PolyCurve*>(curve_2d); //checked
+//	//auto nurbs = dynamic_cast<ON_NurbsCurve*>(curve_2d); //checked
+//
+//	//if (curve_proxy != nullptr) {
+//	//	curve_proxy_c++;
+//	//}
+//	//if (curve_on_surface != nullptr) {
+//	//	on_surface_c++;
+//	//}
+//	//if (line != nullptr) {
+//	//	line_c++;
+//	//}
+//	//if (arc != nullptr) {
+//	//	arc_c++;
+//	//}
+//	//if (polyline != nullptr) {
+//	//	polyline_c++;
+//	//}
+//	//if (polycurve != nullptr) {
+//	//	polycurve_c++;
+//	//}
+//	//if (nurbs != nullptr) {
+//	//	nurbs_c++;
+//	//}
+//
+//
+//
+//	return 0;
+//}
+
+TopoDS_Edge convert_opennurbs_trim_to_occt_edge(ON_BrepTrim* trim, TopoDS_Face occt_face) {
 	BRep_Builder brep_builder;
-	
-	auto occt_curve = convert_opennurbs_curve_to_occt_curve(edge->NurbsCurve());
-
-	TopoDS_Edge occt_edge = BRepBuilderAPI_MakeEdge(occt_curve);
-
-	auto p0 = edge->Vertex(0)->Point();
-	auto p1 = edge->Vertex(1)->Point();
-
-
-
-	//cout << edge->ProxyCurveIsReversed() << endl;
-
-	//马勒戈壁的。
-	//loop中边的顺序可能会有反的。
-	//而且看起来一定会有。
-	//原因是在trim层，可能把3d curve反转。
-	//问题仍然没有解决。
-
-	TopoDS_Vertex occt_v0 = BRepBuilderAPI_MakeVertex(gp_Pnt(p0.x, p0.y, p0.z));
-	TopoDS_Vertex occt_v1 = BRepBuilderAPI_MakeVertex(gp_Pnt(p1.x, p1.y, p1.z));
-	brep_builder.Add(occt_edge, occt_v0);
-	brep_builder.Add(occt_edge, occt_v1);
-
-	//cout << p0.x+300 << " " << p0.y+2200 << " " << 0 << endl;
-	//cout << p1.x+300 << " " << p1.y+2200 << " " << 0 << endl;
-	//cout<<endl;
-
-
-	if (reverse) {
-		occt_edge.Reverse();
-	}
-
-	
-	return occt_edge;
-}
-
-static int curve_proxy_c = 0;
-static int on_surface_c = 0;
-static int line_c = 0;
-static int arc_c = 0;
-static int polyline_c = 0;
-static int polycurve_c = 0;
-static int nurbs_c = 0;
-
-int opennurbs_curve2d_tester(ON_Curve* curve_2d) {
-
-	auto curve_proxy = dynamic_cast<ON_CurveProxy*>(curve_2d); //checked
-	auto curve_on_surface = dynamic_cast<ON_CurveOnSurface*>(curve_2d); //checked
-	auto line = dynamic_cast<ON_LineCurve*>(curve_2d); //checked
-	auto arc = dynamic_cast<ON_ArcCurve*>(curve_2d); //checked
-	auto polyline = dynamic_cast<ON_PolylineCurve*>(curve_2d); //checked
-	auto polycurve = dynamic_cast<ON_PolyCurve*>(curve_2d); //checked
-	auto nurbs = dynamic_cast<ON_NurbsCurve*>(curve_2d); //checked
-
-	if (curve_proxy != nullptr) {
-		curve_proxy_c++;
-	}
-	if (curve_on_surface != nullptr) {
-		on_surface_c++;
-	}
-	if (line != nullptr) {
-		line_c++;
-	}
-	if (arc != nullptr) {
-		arc_c++;
-	}
-	if (polyline != nullptr) {
-		polyline_c++;
-	}
-	if (polycurve != nullptr) {
-		polycurve_c++;
-	}
-	if (nurbs != nullptr) {
-		nurbs_c++;
-	}
-	return 0;
-}
-
-TopoDS_Edge convert_opennurbs_trim_to_occt_edge(ON_BrepTrim* trim) {
-	BRep_Builder occt_brep_builder;
 
 	auto brep = trim->Brep();
 	auto edge = trim->Edge();
+
+	auto interval = trim->Domain();
 	if (trim->m_c2i != -1) {
 		auto curve_2d = brep->m_C2[trim->m_c2i];
-		opennurbs_curve2d_tester(curve_2d);
-	}
-	else {
-		cout <<"no 2d curve"<<endl;
-	}
+		auto nurbs = dynamic_cast<ON_NurbsCurve*>(curve_2d); //checked
+		if (nurbs == nullptr) {
+			nurbs = curve_2d->NurbsCurve();
+		}
 
+		auto occt_bspline_2d = convert_opennurbs_2d_curve_to_occt_2d_curve(nurbs);
+		
+		auto p0 = trim->Vertex(0)->Point();
+		auto p1 = trim->Vertex(1)->Point();
 
-	if (edge != nullptr) {
-		auto occt_edge = convert_opennurbs_edge_to_occt_edge(edge, trim->m_bRev3d);
+		gp_Pnt occt_p0(p0.x, p0.y, p0.z);
+		gp_Pnt occt_p1(p1.x, p1.y, p1.z);
+
+		TopoDS_Vertex occt_v0, occt_v1;
+
+		brep_builder.MakeVertex(occt_v0, occt_p0, Precision::Confusion());
+		brep_builder.MakeVertex(occt_v1, occt_p0, Precision::Confusion());
+
+		TopoDS_Edge occt_edge;
+
+		if (edge != nullptr) {
+			auto occt_bspline = convert_opennurbs_curve_to_occt_curve(edge->NurbsCurve());
+			brep_builder.MakeEdge(occt_edge, occt_bspline, Precision::Confusion());
+		}
+		else {
+			brep_builder.MakeEdge(occt_edge);
+		}
+
+		brep_builder.UpdateEdge(occt_edge, occt_bspline_2d, occt_face, Precision::Confusion());
+		brep_builder.Add(occt_edge, occt_v0);
+		brep_builder.Add(occt_edge, occt_v1);
+		
+		brep_builder.Range(occt_edge, interval.Min(), interval.Max());
+
+		//cout<<endl;
+		//cout<< trim->m_bRev3d <<endl;
+		//cout<< interval.Min()  << " "<< interval.Max() <<endl;
+		
+		if (trim->m_bRev3d) {
+			occt_edge.Reverse();
+			brep_builder.Range(occt_edge, -interval.Max(), -interval.Min(), true);
+		}
+
 		return occt_edge;
 	}
 	else {
-		cout << "trim type:" << trim->m_type << endl;
+		cout <<"no 2d curve"<<endl;
 		return TopoDS_Edge();
 	}
 }
 
-TopoDS_Wire convert_opennurbs_loop_to_occt_wire(const ON_BrepLoop* loop) {
+TopoDS_Wire convert_opennurbs_loop_to_occt_wire(const ON_BrepLoop* loop, TopoDS_Face& occt_face) {
 	BRep_Builder occt_brep_builder;
 
 	TopoDS_Wire occt_wire;
@@ -486,7 +572,7 @@ TopoDS_Wire convert_opennurbs_loop_to_occt_wire(const ON_BrepLoop* loop) {
 
 	for (int i = 0; i < loop->TrimCount(); i++) {
 		if (loop->Trim(i)->Edge() != nullptr) {
-			auto occt_edge = convert_opennurbs_trim_to_occt_edge(loop->Trim(i));
+			auto occt_edge = convert_opennurbs_trim_to_occt_edge(loop->Trim(i), occt_face);
 			if (wire_init) {
 				occt_brep_builder.Add(occt_wire, occt_edge);
 			}
@@ -508,34 +594,36 @@ TopoDS_Wire convert_opennurbs_loop_to_occt_wire(const ON_BrepLoop* loop) {
 }
 
 TopoDS_Face convert_opennurbs_face_to_occt_face(const ON_BrepFace& face) {
-	BRep_Builder occt_brep_builder;
+	BRep_Builder brep_builder;
 
 	//这里遇到个问题。一定要先加外环。
 	//如果直接遍历环往上加，会出现面裁剪出问题的情况。
 	auto occt_surface = convert_opennurbs_surface_to_occt_surface(face.NurbsSurface());
+	TopoDS_Face occt_face;// = BRepBuilderAPI_MakeFace(occt_surface, Precision::Confusion());
+	brep_builder.MakeFace(occt_face, occt_surface, Precision::Confusion());
 
 	if (face.OuterLoop()) {
 		auto outer_loop = face.OuterLoop();
-		auto occt_outer_wire = convert_opennurbs_loop_to_occt_wire(outer_loop);
+		auto occt_outer_wire = convert_opennurbs_loop_to_occt_wire(outer_loop,occt_face);
 
-		TopoDS_Face occt_face = BRepBuilderAPI_MakeFace(occt_surface, occt_outer_wire);
+		brep_builder.Add(occt_face, occt_outer_wire);
 
 		for (int i = 0; i < face.LoopCount(); i++) {
 			if (face.Loop(i) == outer_loop) {
 				continue;
 			}
-			auto occt_wire = convert_opennurbs_loop_to_occt_wire(face.Loop(i));
-			occt_face = BRepBuilderAPI_MakeFace(occt_surface, occt_wire);
+			auto occt_wire = convert_opennurbs_loop_to_occt_wire(face.Loop(i), occt_face);
+			//occt_face = BRepBuilderAPI_MakeFace(occt_surface, occt_wire);
+			brep_builder.Add(occt_face, occt_wire);
 		}
 
 		return occt_face;
 	}
 	else {
-
-		TopoDS_Face occt_face = BRepBuilderAPI_MakeFace(occt_surface, Precision::Confusion());
 		for (int i = 0; i < face.LoopCount(); i++) {
-			auto occt_wire = convert_opennurbs_loop_to_occt_wire(face.Loop(i));
-			occt_face = BRepBuilderAPI_MakeFace(occt_surface, occt_wire);
+			auto occt_wire = convert_opennurbs_loop_to_occt_wire(face.Loop(i) , occt_face);
+			//occt_face = BRepBuilderAPI_MakeFace(occt_surface, occt_wire);
+			brep_builder.Add(occt_face, occt_wire);
 		}
 
 		return occt_face;
@@ -599,7 +687,7 @@ int main() {
 	ON::Begin();
 
 	ONX_Model model;
-	FILE* archive_fp = ON::OpenFile("full.3dm", "rb");
+	FILE* archive_fp = ON::OpenFile("small6.3dm", "rb");
 	ON_BinaryFile archive(ON::read3dm, archive_fp);
 	bool rc = model.Read(archive);
 	if (rc) {
@@ -611,6 +699,11 @@ int main() {
 		return -1;
 	}
 	ON::CloseFile(archive_fp);
+
+	//ON_TextLog log;
+	//model.Dump(log);
+
+	
 
 	BRep_Builder occt_brep_builder;
 	TopoDS_Compound occt_compound_all;
@@ -637,18 +730,18 @@ int main() {
 	ON::End();
 
 
-	cout << "curve_proxy: " << curve_proxy_c << endl;
-	cout << "on_surface: " << on_surface_c << endl;
-	cout << "line: " << line_c << endl;
-	cout << "arc: " << arc_c << endl;
-	cout << "polyline: " << polyline_c << endl;
-	cout << "polycurve: " << polycurve_c << endl;
-	cout << "nurbs: " << nurbs_c << endl;
+	//cout << "curve_proxy: " << curve_proxy_c << endl;
+	//cout << "on_surface: " << on_surface_c << endl;
+	//cout << "line: " << line_c << endl;
+	//cout << "arc: " << arc_c << endl;
+	//cout << "polyline: " << polyline_c << endl;
+	//cout << "polycurve: " << polycurve_c << endl;
+	//cout << "nurbs: " << nurbs_c << endl;
 	
-	BRepTools::Write(occt_compound_all, "full.brep");
+	BRepTools::Write(occt_compound_all, "small6.brep");
 	
-	system("pause");
-	return -2;
+	//system("pause");
+	//return -2;
 
 	Bnd_Box b;
 
@@ -682,7 +775,7 @@ int main() {
 		}
 	}
 	
-	write_obj(vs, fs, "full.obj");
+	write_obj(vs, fs, "small6.obj");
 
 	ON::End();
 	system("pause");
